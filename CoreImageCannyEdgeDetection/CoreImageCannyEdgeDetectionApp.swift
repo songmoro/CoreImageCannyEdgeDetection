4c28d13e96dff2175897849f654ce71896e73b54
songmoro <wlqgkr@naver.com>
2023-12-21 00:43:03 +0900

    Initial Commit

---
+++ CoreImageCannyEdgeDetection/CoreImageCannyEdgeDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreImageCannyEdgeDetectionApp.swift
+//  CoreImageCannyEdgeDetection
+//
+//  Created by 송재훈 on 12/21/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreImageCannyEdgeDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
