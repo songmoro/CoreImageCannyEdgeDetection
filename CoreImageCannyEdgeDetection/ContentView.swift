e99dec8f7ac92452da0a663a7213a334c7a49970
songmoro <wlqgkr@naver.com>
2023-12-21 00:47:32 +0900

    feat: apply Canny edge detector

--- CoreImageCannyEdgeDetection/ContentView.swift
+++ CoreImageCannyEdgeDetection/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var image = UIImage(named: "lena")!
+    let cannyEdgeDetector = CannyEdgeDetector()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.black.ignoresSafeArea()
+            
+            VStack {
+                Image(uiImage: image)
+                
+                Button {
+                    image = cannyEdgeDetector.detectEdges(image)
+                } label: {
+                    Text("Canny Edge Detect")
+                }
+            }
+            
         }
-        .padding()
     }
 }
 
